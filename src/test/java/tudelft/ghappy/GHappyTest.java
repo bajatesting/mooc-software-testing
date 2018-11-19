1596907c8ff9b57a368a050adfc32fcd67914111
bb <bb@com.pl>
2018-11-19 21:04:58 +0100

    Ghappy bug fixing and parametrized test

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,28 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    private GHappy gHappy;
+
+    @BeforeEach
+    public void initializer() {
+       this.gHappy = new GHappy();
+
+    }
+
+    @ParameterizedTest(name="input={0}, result={1}")
+    @CsvSource({"xxggxx, true", "ggcc, true", "gkkg, false", "gggx, true", "ddggcc, true"})
+    public void inputTest(String input, boolean result) {
+        boolean stringVerificatiion = gHappy.gHappy(input);
+        Assertions.assertEquals(result, stringVerificatiion);
+
+
+
+    }
 }
